1efa257388205b7bc1396e40ca4143f5551df12e
JapaneseZgredek <fabian.zbranski@gmail.com>
2024-11-02 00:12:23 +0100

    Create ShipItem and ShipList

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,8 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from pathlib import Path
 
 app = FastAPI()
-
-# Mount frontend as static files
-app.mount('/static', StaticFiles(directory='frontend/static'), name='static')
 
 @app.get('/', response_class=HTMLResponse)
 async def read_index():
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavbarComponent from './components/Navbar/Navbar';
+import ShipList from "./components/Ship/ShipList";
 
 const App = () => {
   return (
     <div className="App">
       <NavbarComponent />
+        <ShipList />
     </div>
   );
 };
